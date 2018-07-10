bb2443129845a8ae6fc196ac6290f7e4a40d892b
Emory Dunn <emory@emorydunn.com>
2018-07-09 20:41:41 -0700

    Clean up main

--- Sources/gps2location/main.swift
+++ Sources/gps2location/main.swift
@@ -5,41 +5,27 @@ import Basic
 
 let version = Version(0, 1, 0)
 
-do {
-    let parser = OptionParser(arguments: Array(CommandLine.arguments.dropFirst()))
-    let options = parser.options
-    
-    if options.shouldPrintVersion {
-        print("gps2location \(version)")
-        exit(0)
-    }
-    
-    let geocoder: ReverseGeocoder
-    switch options.api {
-    case .apple:
-        geocoder = AppleGeocoder()
-    case .google:
-        geocoder = GoogleGeocoder()
-    }
 
-    let updater = LocationUpdater(sourceURLs: options.input, geocoder: geocoder, exiftool: Exiftool(), dryRun: options.shouldPerformDryRun)
-    
-    updater.update { (_, _) in
-        
-    }
-    
-//    let terminalController = TerminalController(stream: stdoutStream as! LocalFileOutputByteStream)
+let parser = OptionParser(arguments: Array(CommandLine.arguments.dropFirst()))
+let options = parser.options
 
-//    let readBar = createProgressBar(forStream: stdoutStream, header: "Reading EXIF")
-//    let readBar = ProgressBar(
-//    readBar.
+if options.shouldPrintVersion {
+    print("gps2location \(version)")
+    exit(0)
+}
+
+let geocoder: ReverseGeocoder
+switch options.api {
+case .apple:
+    geocoder = AppleGeocoder()
+case .google:
+    geocoder = GoogleGeocoder()
+}
+let exiftool = Exiftool(trace: nil)
+
+let updater = LocationUpdater(sourceURLs: options.input, geocoder: geocoder, exiftool: exiftool, dryRun: options.shouldPerformDryRun)
+
+updater.update { (success, total) in
+    print("Updated \(success) / \(total)")
     
-//    try updater.update() { success, total in
-//        print("Updated \(success)/\(total)")
-//        CFRunLoopStop(CFRunLoopGetMain())
-//    }
-//    CFRunLoopRun()
-    
-} catch {
-    print(error)
 }
